456fd8efb3fe1f3d691542f7a8b82eac07e9f8c6
jhonsalcedo1 <jhon.salcedo1@unmsm.edu.pe>
2022-09-06 11:33:18 -0500

    nuevo

--- clases.js
+++ clases.js
@@ -1,7 +1,21 @@
 class Persona {
+
+    static contadorPersonas = 0;
+
+    static get MAX_OBJ() {
+        return 5;
+    }
+
     constructor(nombre, apellido) {
         this._nombre = nombre;
         this._apellido = apellido;
+        if (Persona.contadorPersonas < Persona.MAX_OBJ) {
+            this.idPersona = ++Persona.contadorPersonas;
+        } else {
+            console.log("Se han superado el maximo de objetos permitidos ");
+        }
+        this.idPersona = Persona.contadorPersonas++;
+
     }
     get nombre() {
         return this._nombre;
@@ -17,10 +31,17 @@ class Persona {
         return this._apellido = apellido;
     }
     nombreCompleto() {
-        return this._nombre + "" + this._apellido;
+        return this.idPersona + " " + this._nombre + "" + this._apellido;
     }
     toString() {
         return this.nombreCompleto();
+    }
+    static saludar() {
+        console.log("saludos desde metodos static")
+    }
+    static saludar2(persona) {
+        console.log(persona.nombre);
+
     }
 }
 
@@ -42,11 +63,15 @@ class Empleado extends Persona {
     }
 }
 let persona1 = new Persona("Juan", "Perez");
-console.log(persona1);
-console.log(persona1.nombreCompleto());
+console.log(persona1.toString());
 
 let empleado1 = new Empleado("Maria", "Jimenez", "Sistemas");
-console.log(empleado1);
-console.log(empleado1.nombre);
-console.log(empleado1.nombreCompleto());
 console.log(empleado1.toString());
+
+console.log(Persona.contadorPersonas);
+
+let persona2 = new Persona("Karla", "Ramirez");
+console.log(Persona.contadorPersonas);
+console.log(persona2.toString);
+
+console.log(Persona.MAX_OBJ);
